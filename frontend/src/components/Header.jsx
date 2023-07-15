798bb2e850766d11b25f030f80a9c6655c69aa83
sunnextt <sunnext2010@gmail.com>
2023-07-15 17:46:38 +0100

    dev commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -71,19 +71,7 @@ const Header = () => {
               )}
 
               {/* Admin Links */}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <LinkContainer to='/admin/productlist'>
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/orderlist'>
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/userlist'>
-                    <NavDropdown.Item>Users</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )}
+              {userInfo && userInfo.isAdmin && ("hello" )}
             </Nav>
           </Navbar.Collapse>
         </Container>
